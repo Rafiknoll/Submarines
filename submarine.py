6cd61a94ecde775e74134a9229c1a66368886c9b
Rafael Knoll <Rafihknoll@gmail.com>
2020-12-30 10:49:48 +0200

    Documented submarine.py

--- submarine.py
+++ submarine.py
@@ -1,13 +1,28 @@
+"""
+Name:       submarine.py
+
+Purpose:    Provides a class to hold the information on a submarine
+
+Usage:      from submarine import Submarine
+
+Author:     Rafael Knoll
+"""
 from enum import Enum
 from consts import BOARD_SIZE
 
 
 class SubmarineState(Enum):
+    """
+    Represents the state in which each location of the submarine can be
+    """
     UNDETECTED = 0
     HIT = 1
 
 
 class Submarine:
+    """
+    Provides easy means to manage submarines and check on them
+    """
 
     def __init__(self, *locations):
         self.locations = {}
@@ -21,12 +36,22 @@ class Submarine:
             self.locations[location] = SubmarineState.UNDETECTED
 
     def hit_location(self, location):
+        """
+        Hits the target location of the submarine
+        :param location: The location to hit
+        :return: None
+        :raises: KeyError if this location is not on the submarine
+        """
         if location in self.locations.keys():
             self.locations[location] = SubmarineState.HIT
         else:
             raise KeyError(f"This submarine doesn't exist in location {location}")
 
     def is_dead(self):
+        """
+        Checks if the submarine is dead
+        :return: True if it is dead
+        """
         for state in self.locations.values():
             if state == SubmarineState.UNDETECTED:
                 return False
