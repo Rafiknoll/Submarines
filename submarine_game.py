bad762f1bc4e3126cf15da2474651f13e94e8c9a
Rafael Knoll <Rafihknoll@gmail.com>
2020-12-30 10:38:32 +0200

    Documented submarine_game.py

--- submarine_game.py
+++ submarine_game.py
@@ -1,3 +1,12 @@
+"""
+Name:       submarine_game.py
+
+Purpose:    The runnable of the submarine game
+
+Usage:      python submarine_game.py [target_ip]
+
+Author:     Rafael Knoll
+"""
 from game_manager import GameManager
 from board import BoardManager
 from online_client import OnlineClient
@@ -7,6 +16,11 @@ TARGET_IP_ARGV_INDEX = 1
 
 
 def choose_online_mode():
+    """
+    Decides whenever to use the passive listening mode or actively connect to someone depending if there was an ip
+    given as parameter or not
+    :return: The online client for the game
+    """
     if len(sys.argv) > 1:
         print(f"Connecting to {sys.argv[TARGET_IP_ARGV_INDEX]}")
         return OnlineClient(sys.argv[TARGET_IP_ARGV_INDEX])
@@ -16,6 +30,10 @@ def choose_online_mode():
 
 
 def main():
+    """
+    Creates the board and online client for the game manager, connects to a player then runs the game
+    :return: None
+    """
     board = BoardManager()
     online_client = choose_online_mode()
     online_client.match_with_another_player()
