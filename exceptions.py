1d42c6096194ce185ed62ac15c78f1ff57e30e7f
Rafael Knoll <Rafihknoll@gmail.com>
2020-12-30 10:52:39 +0200

    Documented exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,37 @@
+"""
+Name:       exceptions.py
+
+Purpose:    Provides dedicated exceptions for the submarine game
+
+Usage:      from exceptions import *
+
+Author:     Rafael Knoll
+"""
+
+
 class LocationOccupiedException(Exception):
+    """
+    Occurs when attempting to place a submarine in an occupied location
+    """
     pass
 
 
 class ConnectionNotMadeYetException(Exception):
+    """
+    Occurs when an attempt to use a connection is made before the connection itself was made
+    """
     pass
 
 
 class EnemySurrenderedException(Exception):
+    """
+    Occurs when the enemy surrenders in the middle of the game
+    """
     pass
 
 
 class SelfSurrenderException(Exception):
+    """
+    Occurs when the player surrenders in the middle of the game
+    """
     pass
